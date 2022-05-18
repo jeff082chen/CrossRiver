06c1dfedcdc701a7f13443c9bd157f676792a73e
Jeffrey <jeff082chen@gmail.com>
2022-05-18 13:06:14 +0800

    ver 2.0
    basic function complete

--- GUI/app.py
+++ GUI/app.py
@@ -1,5 +1,9 @@
+from itertools import accumulate
+import sys
 import time
+import traceback
 import tkinter as tk
+import tkinter.messagebox as messagebox
 from PIL import Image, ImageTk
 from CrossRiver.setup import setParameter
 
@@ -10,6 +14,9 @@ div_size = 200
 align_mode = 'nswe' # align center
 pad = 3
 
+class SolutionNotFoundError(Exception):
+    pass
+
 class Root(tk.Tk):
     def __init__(self):
         super().__init__()
@@ -18,15 +25,21 @@ class Root(tk.Tk):
 
 class ControlSystem:
     def __init__(self):
-        self.ready = False
+        self.ready_to_search = True
+        self.ready_to_move = False
+        self.ready_to_reset = False
         self.lock = False
 
         self.path = None
+        self.total_time = None
+        self.total_price = None
+        self.time_list = []
+        self.price_list = []
 
         self.boatA_pos = 1
         self.boatB_pos = 1
-        self.right_cannibals = 3
-        self.right_missionaries = 3
+        self.right_cannibals = 0
+        self.right_missionaries = 0
         self.left_cannibals = 0
         self.left_missionaries = 0
         self.boatA_cannibals = 0
@@ -92,22 +105,35 @@ class ControlSystem:
         self.boatB_cannibals = 0
         self.boatB_missionaries = 0
 
-    def do_search(self, N, M, mode):
-        Node = setParameter(N, M, mode = mode, alg = 'AS')
+    def do_search(self, N, M, mode, limit):
+        Node = setParameter(N, M, mode = mode, alg = 'AS', limit = limit)
         start = Node()
         result = start.AStartSearch()
+
+        if result is None:
+            raise SolutionNotFoundError
+
         self.path = [step.move for step in result['path'][1:]]
-        self.ready = True
+        self.time_list = [step.total_time for step in result['path'][1:]]
+        self.price_list = [step.total_price for step in result['path'][1:]]
+        self.total_time = self.total_price = 0
     
     def reset(self):
-        self.right_cannibals = 3
-        self.right_missionaries = 3
+        self.right_cannibals = 0
+        self.right_missionaries = 0
         self.left_cannibals = 0
         self.left_missionaries = 0
         self.boatA_cannibals = 0
         self.boatA_missionaries = 0
         self.boatB_cannibals = 0
         self.boatB_missionaries = 0
+
+        self.total_price = None
+        self.total_time = None
+
+    def update(self, N, M):
+        self.right_cannibals = N
+        self.right_missionaries = N + M
 
 
 
@@ -125,17 +151,46 @@ class App:
         self.root_div2.grid(column = 0, row = 1, sticky = align_mode)
 
         # create items in root_div1
-        self.search_button = tk.Button(root, text = 'search', bg = 'green')
+        self.state_label = tk.Label(self.root_div1, text = 'ready to search', font = ('Arial', 20), bg = 'white')
+        self.search_button = tk.Button(self.root_div1, text = 'search', bg = 'white')
         self.search_button.bind('<Button-1>', self.search)
-        self.move_button = tk.Button(root, text = 'move', bg = 'white')
+        self.move_button = tk.Button(self.root_div1, text = 'move', bg = 'white')
         self.move_button.bind('<Button-1>', self.move)
-        self.reset_button = tk.Button(root, text = 'reset', bg = 'white')
+        self.reset_button = tk.Button(self.root_div1, text = 'reset', bg = 'white')
         self.reset_button.bind('<Button-1>', self.reset)
+        self.quit_button = tk.Button(self.root_div1, text = 'quit', bg = 'white')
+        self.quit_button.bind('<Button-1>', self.quit)
+
+        self.N_label = tk.Label(self.root_div1, text = 'N:', font = ('Arial', 20), bg = 'white')
+        self.N_input = tk.Text(self.root_div1, width = 3, height = 1, font = ('Arial', 12), bg = 'white', highlightbackground = "black", highlightthickness = 2)
+        self.M_label = tk.Label(self.root_div1, text = 'M:', font = ('Arial', 20), bg = 'white')
+        self.M_input = tk.Text(self.root_div1, width = 3, height = 1, font = ('Arial', 12), bg = 'white', highlightbackground = "black", highlightthickness = 2)
+        self.mode_label = tk.Label(self.root_div1, text = 'mode:', font = ('Arial', 20), bg = 'white')
+        self.mode_input = tk.Text(self.root_div1, width = 3, height = 1, font = ('Arial', 12), bg = 'white', highlightbackground = "black", highlightthickness = 2)
+        self.limit_lable = tk.Label(self.root_div1, text = 'limit:', font = ('Arial', 20), bg = 'white')
+        self.limit_input = tk.Text(self.root_div1, width = 3, height = 1, font = ('Arial', 12), bg = 'white', highlightbackground = "black", highlightthickness = 2)
+
+        self.price_label = tk.Label(self.root_div1, text = 'price:', font = ('Arial', 20), bg = 'white')
+        self.time_label = tk.Label(self.root_div1, text = 'time:', font = ('Arial', 20), bg = 'white')
 
         # display items in root_div1
-        self.search_button.place(x = 50, y = 50, width = 80, height = 20)
-        self.move_button.place(x = 50, y = 80, width = 80, height = 20)
-        self.reset_button.place(x = 50, y = 110, width = 80, height = 20)
+        self.search_button.place(x = 50, y = 20, width = 80, height = 20)
+        self.move_button.place(x = 50, y = 50, width = 80, height = 20)
+        self.reset_button.place(x = 50, y = 80, width = 80, height = 20)
+        self.quit_button.place(x = 50, y = 110, width = 80, height = 20)
+        self.state_label.place(x = 120, y = 110, width = 200, height = 20)
+
+        self.N_label.place(x = 210, y = 20, width = 20, height = 20)
+        self.N_input.place(x = 250, y = 20, width = 50, height = 20)
+        self.M_label.place(x = 210, y = 60, width = 20, height = 20)
+        self.M_input.place(x = 250, y = 60, width = 50, height = 20)
+        self.mode_label.place(x = 360, y = 20, width = 80, height = 20)
+        self.mode_input.place(x = 450, y = 20, width = 80, height = 20)
+        self.limit_lable.place(x = 360, y = 60, width = 80, height = 20)
+        self.limit_input.place(x = 450, y = 60, width = 80, height = 20)
+
+        self.price_label.place(x = 600, y = 20, width = 120, height = 20)
+        self.time_label.place(x = 600, y = 60, width = 120, height = 20)
 
         # create items in root_div2
         self.canvas = tk.Canvas(self.root_div2, width = div_size * 5, height = div_size * 2.8) # 560 * 1000
@@ -147,14 +202,14 @@ class App:
         self.bg = self.canvas.create_image(0, 0, anchor = tk.NW, image = self.river_img)
         self.boatA = self.canvas.create_image(div_size * 3.5, div_size * 0.7, image = self.boat_img)
         self.boatB = self.canvas.create_image(div_size * 3.5, div_size * 2.1, image = self.boat_img)
-        self.right_cannibals = self.canvas.create_text(div_size * 4.5, div_size * 0.7, text = str(self.control.right_cannibals), font = ('Arial', 100), fill = 'black')
-        self.right_missionaries = self.canvas.create_text(div_size * 4.5, div_size * 2.1, text = str(self.control.right_missionaries), font = ('Arial', 100), fill = 'Crimson')
-        self.left_cannibals = self.canvas.create_text(div_size * 0.5, div_size * 0.7, text = str(self.control.left_cannibals), font = ('Arial', 100), fill = 'black')
-        self.left_missionaries = self.canvas.create_text(div_size * 0.5, div_size * 2.1, text = str(self.control.left_missionaries), font = ('Arial', 100), fill = 'Crimson')
-        self.boatA_cannibals = self.canvas.create_text(div_size * 3.3, div_size * 0.4, text = str(self.control.boatA_cannibals), font = ('Arial', 50), fill = 'black')
-        self.boatA_missionaries = self.canvas.create_text(div_size * 3.6, div_size * 0.4, text = str(self.control.boatA_missionaries), font = ('Arial', 50), fill = 'Crimson')
-        self.boatB_cannibals = self.canvas.create_text(div_size * 3.3, div_size * 1.8, text = str(self.control.boatB_cannibals), font = ('Arial', 50), fill = 'black')
-        self.boatB_missionaries = self.canvas.create_text(div_size * 3.6, div_size * 1.8, text = str(self.control.boatB_missionaries), font = ('Arial', 50), fill = 'Crimson')
+        self.right_cannibals = self.canvas.create_text(div_size * 4.5, div_size * 0.7, text = str(self.control.right_cannibals), font = ('Arial', 100), fill = 'Crimson')
+        self.right_missionaries = self.canvas.create_text(div_size * 4.5, div_size * 2.1, text = str(self.control.right_missionaries), font = ('Arial', 100), fill = 'black')
+        self.left_cannibals = self.canvas.create_text(div_size * 0.5, div_size * 0.7, text = str(self.control.left_cannibals), font = ('Arial', 100), fill = 'Crimson')
+        self.left_missionaries = self.canvas.create_text(div_size * 0.5, div_size * 2.1, text = str(self.control.left_missionaries), font = ('Arial', 100), fill = 'black')
+        self.boatA_cannibals = self.canvas.create_text(div_size * 3.3, div_size * 0.4, text = str(self.control.boatA_cannibals), font = ('Arial', 50), fill = 'Crimson')
+        self.boatA_missionaries = self.canvas.create_text(div_size * 3.6, div_size * 0.4, text = str(self.control.boatA_missionaries), font = ('Arial', 50), fill = 'black')
+        self.boatB_cannibals = self.canvas.create_text(div_size * 3.3, div_size * 1.8, text = str(self.control.boatB_cannibals), font = ('Arial', 50), fill = 'Crimson')
+        self.boatB_missionaries = self.canvas.create_text(div_size * 3.6, div_size * 1.8, text = str(self.control.boatB_missionaries), font = ('Arial', 50), fill = 'black')
 
     def update(self):
         self.canvas.itemconfig(self.right_cannibals, text = str(self.control.right_cannibals))
@@ -166,8 +221,11 @@ class App:
         self.canvas.itemconfig(self.boatB_cannibals, text = str(self.control.boatB_cannibals))
         self.canvas.itemconfig(self.boatB_missionaries, text = str(self.control.boatB_missionaries))
 
+        self.time_label.config(text = 'time: ' + (str(self.control.total_time) if self.control.total_time is not None else ''))
+        self.price_label.config(text = 'price: ' + (str(self.control.total_price) if self.control.total_price is not None else ''))
+
     def moving(self, boatA_step, boatB_step):
-        for i in range(20):
+        for _ in range(20):
             self.canvas.move(self.boatA, boatA_step, 0)
             self.canvas.move(self.boatA_cannibals, boatA_step, 0)
             self.canvas.move(self.boatA_missionaries, boatA_step, 0)
@@ -197,27 +255,71 @@ class App:
         self.control.check_validity()
         self.update()
 
+    def get_parameters(self):
+        N = self.N_input.get("1.0", "end").strip()
+        M = self.M_input.get("1.0", "end").strip()
+        mode = self.mode_input.get("1.0", "end").strip()
+        limit = self.limit_input.get("1.0", "end").strip()
+        return N, M, mode, limit
+
     def search(self, event = None):
-        if self.control.lock or self.control.ready:
+        if self.control.lock or not self.control.ready_to_search:
             return
         self.control.lock = True
-        self.search_button['bg'] = 'red'
-        self.control.do_search(3, 0, 'p')
-        self.search_button['bg'] = 'green'
-        self.control.lock = False
+        N, M, mode, limit = self.get_parameters()
+
+        if mode == 'price':
+            mode = 'p'
+        elif mode == 'time':
+            mode = 't'
+
+        if limit == '':
+            limit = None
+
+        try:
+            N = int(N)
+            M = int(M)
+            limit = int(limit) if limit else None
+            self.control.update(N, M)
+            self.control.do_search(N, M, mode, limit)
+        except SolutionNotFoundError:
+            messagebox.showinfo('Error', 'No solution found')
+        except Exception as e:
+            print(N, M, mode, limit)
+            error_class = e.__class__.__name__ #取得錯誤類型
+            detail = e.args[0] #取得詳細內容
+            cl, exc, tb = sys.exc_info() #取得Call Stack
+            lastCallStack = traceback.extract_tb(tb)[-1] #取得Call Stack的最後一筆資料
+            fileName = lastCallStack[0] #取得發生的檔案名稱
+            lineNum = lastCallStack[1] #取得發生的行號
+            funcName = lastCallStack[2] #取得發生的函數名稱
+            errMsg = "File \"{}\", line {}, in {}: [{}] {}".format(fileName, lineNum, funcName, error_class, detail)
+            print("\nExecution halted:\n" + errMsg)
+            self.state_label.config(text = "Error, try again")
+        else:
+            self.update()
+            self.state_label.config(text = 'ready to move')
+            self.control.ready_to_search = False
+            self.control.ready_to_move = True
+        finally:
+            self.control.lock = False
 
     def move(self, event = None):
-        if self.control.lock or not self.control.ready:
+        if self.control.lock or not self.control.ready_to_move:
             return
         self.control.lock = True
-        for step in self.control.path:
+        for i, step in enumerate(self.control.path):
+            self.control.total_price = self.control.price_list[i]
+            self.control.total_time = self.control.time_list[i]
+            self.update()
             self.move_boats(int(step[0], 16), int(step[1], 16), int(step[2], 16), int(step[3], 16))
-        self.path = None
-        self.control.ready = False
+        self.state_label.config(text = 'ready to reset')
+        self.control.ready_to_move = False
+        self.control.ready_to_reset = True
         self.control.lock = False
 
     def reset(self, event = None):
-        if self.control.lock or self.control.ready:
+        if self.control.lock or not self.control.ready_to_reset:
             return
         self.control.lock = True
         self.control.reset()
@@ -227,7 +329,13 @@ class App:
         self.moving(boatA_step, boatB_step)
         self.control.boatA_pos = 1
         self.control.boatB_pos = 1
+        self.state_label.config(text = 'ready to search')
+        self.control.ready_to_reset = False
+        self.control.ready_to_search = True
         self.control.lock = False
+
+    def quit(self, event = None):
+        self.root.destroy()
 
 
 
--- README.md
+++ README.md
@@ -11,6 +11,10 @@ $ cd CrossRiver
 # install dependencies
 $ pip install -r requirements.txt
 
+# GUI app
+$ python -m GUI.app
+
+# Command line interface
 # arguments
 # -N: integer between 3~10
 # -M: integer between 0~2
@@ -20,3 +24,17 @@ $ pip install -r requirements.txt
 # mode: -p -> minimum price, -t -> minimun time
 $ python -m CrossRiver.main -N[3..10] -M[0..2] [-t or -p] [-UC or -AS] [-d] [-l[limit]]
 ```
+
+# GUI app
+<img src="https://github.com/jeff082chen/CrossRiver/blob/main/img/app.png" width="500"/>  
+
+1. input parameters 
+    + N: integer between 3~10
+    + M: integer between 0~2
+    + limit: limit of cost (time limit if -p, price limit if -t) (default: no limit)
+    + mode: price (p for short) or time (t for short)
+2. press `search` button
+3. when label shows `ready to move`, press `move` button
+4. wait for the move to finish
+5. when label shows `ready to reset`, press `reset` button
+6. press `quit` button to quit, or press `search` button to restart
